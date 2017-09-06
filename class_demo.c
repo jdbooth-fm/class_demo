36eda28b910e0f648ba4e907c3392d284901d5cb
Joshua Booth <jbooth1@229-STA-S-18369.local>
2017-09-06 13:59:45 -0400

    Added class_demo.h/c

---
+++ class_demo.c
@@ -0,0 +1,13 @@
+#include "class_demo.h"
+
+
+
+int main(int argc, char* argv[])
+{
+
+
+  printf("Hello Class Demo\n");
+  
+
+  return 0;
+}
---
+++ class_demo.h
@@ -0,0 +1,10 @@
+#ifndef CLASS_DEMO_H
+#define CLASS_DEMO_H
+
+
+/*Because we are all C at heart*/
+#include <cstdlib>
+#include <cstdio>
+
+
+#endif
